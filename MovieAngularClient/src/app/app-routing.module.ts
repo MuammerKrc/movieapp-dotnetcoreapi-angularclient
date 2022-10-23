d9ec215e619d5b76ba598b5348afb61128c31a5b
MuammerKrc <muammer03karaca@gmail.com>
2022-10-23 23:07:59 +0300

    added ui components and layout

--- MovieAngularClient/src/app/app-routing.module.ts
+++ MovieAngularClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/ui/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"register",loadChildren:()=>import('./components/ui/register/register.module').then(module=>module.RegisterModule)},
+  {path:"login",loadChildren:()=>import('./components/ui/login/login.module').then(module=>module.LoginModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ MovieAngularClient/src/app/components/ui/ui.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterModule } from './register/register.module';
+import { LoginModule } from './login/login.module';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    HomeModule,
+    RegisterModule,
+    LoginModule
+  ]
+})
+export class UiModule { }

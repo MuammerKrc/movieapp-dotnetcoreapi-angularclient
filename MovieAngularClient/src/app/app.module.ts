d99c1fa9e24bd5a0826d32e4ca2f2169d5a2989d
MuammerKrc <muammer03karaca@gmail.com>
2022-10-24 22:14:41 +0300

    add initial migration

--- MovieAngularClient/src/app/app.module.ts
+++ MovieAngularClient/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { LayoutModule } from './layout/layout.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { LayoutModule } from './layout/layout.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    LayoutModule,
-    ComponentsModule
+    ComponentsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
